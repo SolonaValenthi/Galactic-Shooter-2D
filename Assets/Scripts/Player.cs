d8f0133272a727e26bf770c823c7b6715e96e78f
Nikolai Nelson <nikolainelson@comcast.net>
2023-08-08 05:12:18 -0700

    added transform function to plater script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,8 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Set current position = new position (0, 0, 0)
+        transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
