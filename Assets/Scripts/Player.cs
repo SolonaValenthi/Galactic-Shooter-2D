472e7ff5f0f1c3bd58f58f1df94d03ac30183445
Nikolai Nelson <nikolainelson@comcast.net>
2023-08-08 06:01:58 -0700

    added simple movement to player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,6 +14,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // new Vector3(1, 0, 0) * 5 * real time
+        transform.Translate(Vector3.right * 5 * Time.deltaTime);
     }
 }
