777f18bfd73e8730bbda267f86bd34d26c396816
Nikolai Nelson <nikolainelson@comcast.net>
2023-08-09 07:43:17 -0700

    added player input control for movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,7 +18,14 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // new Vector3(1, 0, 0) * 5 * real time
-        transform.Translate(Vector3.right * _speed * Time.deltaTime);
+        // fetch horizontal input from the input manager
+        float horizontalInput = Input.GetAxis("Horizontal");
+        // fetch vertical input from the input manager
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // new Vector3(1, 0, 0) * player input * 5 * real time
+        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        // new Vector3(0, 1, 0) * player input * 5 * real time
+        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
     }
 }
