d0f4072aae0fad05f556db0267800ecd7fc94f55
Nikolai Nelson <nikolainelson@comcast.net>
2023-08-09 08:52:22 -0700

    code cleanup

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,17 +18,17 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Fetch horizontal input from the input manager
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
-        // Fetch vertical input from the input manager
         float verticalInput = Input.GetAxis("Vertical");
-        // Vector3 to store player movement input
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 
-        // Vector3(playerinput, playerInput, 0) * 3.5f * real time
         transform.Translate(direction * _speed * Time.deltaTime);
 
-        // Restrict player movement on y axis
         if (transform.position.y >= 2)
         {
             transform.position = new Vector3(transform.position.x, 2, 0);
@@ -38,7 +38,6 @@ public class Player : MonoBehaviour
             transform.position = new Vector3(transform.position.x, -3.8f, 0);
         }
 
-        // Wrap player to other side when moving off-screen
         if (transform.position.x > 11.3f)
         {
             transform.position = new Vector3(-11.3f, transform.position.y, 0);
