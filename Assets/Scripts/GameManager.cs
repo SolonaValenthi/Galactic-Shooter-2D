eefac25575a2afa683546cdf95202c168c4bfd08
Nikolai Nelson <nikolainelson@comcast.net>
2023-08-20 05:22:52 -0700

    created main menu scene and ui, and start game button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,7 +12,7 @@ public class GameManager : MonoBehaviour
     {
         if (_isGameOver == true && Input.GetKeyDown(KeyCode.R))
         {
-            SceneManager.LoadScene(0);
+            SceneManager.LoadScene(1); // load game scene
         }
     }
 
