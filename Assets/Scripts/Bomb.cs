8b5eb18c7afe67237e5c577fe92c1611974ed9a6
Nikolai Nelson <nikolainelson@comcast.net>
2023-09-07 11:44:58 -0700

    created bomb asset, animation, and basic behavior

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    private float _rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rotationSpeed = Random.Range(-1.5f, 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, _rotationSpeed);
+    }
+}
