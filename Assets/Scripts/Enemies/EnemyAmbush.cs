74e99ae11b9ab122d20e188fd561c636636e6c23
Nikolai Nelson <nikolainelson@comcast.net>
2023-09-14 01:54:05 -0700

    created ambusher enemy type and relevant script

---
+++ Assets/Scripts/Enemies/EnemyAmbush.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAmbush : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _primaryLaserPrefab;
+    [SerializeField]
+    private GameObject _ambushTargetIndicator;
+    [SerializeField]
+    private GameObject _ambushLaser;
+    [SerializeField]
+    private AudioClip _primaryLaserClip;
+    [SerializeField]
+    private Vector3 _laserOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
