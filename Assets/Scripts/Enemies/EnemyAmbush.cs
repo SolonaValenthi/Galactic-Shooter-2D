9838dcf0f90dcd7ef300c463f2877793ce000d21
Nikolai Nelson <nikolainelson@comcast.net>
2023-09-14 02:48:00 -0700

    defined basic mvoement behavior of ambush enemy type

--- Assets/Scripts/Enemies/EnemyAmbush.cs
+++ Assets/Scripts/Enemies/EnemyAmbush.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EnemyAmbush : MonoBehaviour
 {
     [SerializeField]
+    private float _enemySpeed = 4.0f;
+    [SerializeField]
     private GameObject _primaryLaserPrefab;
     [SerializeField]
     private GameObject _ambushTargetIndicator;
@@ -15,15 +17,93 @@ public class EnemyAmbush : MonoBehaviour
     [SerializeField]
     private Vector3 _laserOffset;
 
+    private float _spawnRange;
+    private float _canFire;
+    private bool _isAmbushing;
+    private bool _flyingIn = true;
+    private GameObject _playerObj;
+    private Vector3 _flyInDirection;
+    private Vector3 _flyInDestination;
+
+    /// ambush enemy behavior outline
+    /// Spawns in and moves like a normal enemy (done)
+    /// can detect and will try to avoid player attacks
+    /// actively tries to avoid collision with player to use ambush weapon
+    /// upon reaching bottom screen bound stop moving
+    /// turn towards and begin tracking the player
+    /// display a target indicator
+    /// cease tracking after a few seconds
+    /// fire a large piercing laser at the final position, shortly after tracking ends
+    /// resume moving off screen to "respawn" at the top bound
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
+        CalculateFlyIn();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_flyingIn == true)
+        {
+            FlyIn();
+        }
+
+        if (_flyingIn == false && _isAmbushing == false)
+        {
+            AmbushMovement();
+            
+            if (transform.position.y <= -5.3f)
+            {
+                _isAmbushing = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
         
+    }
+
+    private void FlyIn()
+    {
+        float vertSpeed = Mathf.Abs(transform.position.y - _flyInDestination.y);
+
+        transform.Translate((Vector3.down * vertSpeed + (_flyInDirection * _enemySpeed)) * Time.deltaTime);
+
+        if (vertSpeed < 1.5f)
+        {
+            _flyingIn = false;
+        }
+    }
+
+    private void CalculateFlyIn()
+    {
+        if (transform.position.x > 0)
+        {
+            _flyInDirection = Vector3.left;
+            _flyInDestination = new Vector3(transform.position.x - Random.Range(4.0f, 10.0f), 1, 0);
+        }
+        else if (transform.position.x < 0)
+        {
+            _flyInDirection = Vector3.right;
+            _flyInDestination = new Vector3(transform.position.x + Random.Range(4.0f, 10.0f), 1, 0);
+        }
+    }
+
+    private void AmbushMovement()
+    {
+        transform.Translate(Vector3.down * _enemySpeed * Time.deltaTime);
+
+        if (transform.position.y < -7)
+        {
+            _spawnRange = Random.Range(-9.0f, 9.0f);
+            transform.position = new Vector3(_spawnRange, 8, 0);
+            CalculateFlyIn();
+            _flyingIn = true;
+        }
     }
 }
--- Assets/Scripts/Enemies/PowerupDetection.cs
+++ Assets/Scripts/Enemies/PowerupDetection.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PowerupDetection : MonoBehaviour
 {
+    private int _parentID; // 0 = basic enemy, 1 = ambush enemy
     private bool _hasTarget = false;
     private GameObject _powerupTarget;
     private Enemy _enemyParent;
@@ -11,18 +12,22 @@ public class PowerupDetection : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _enemyParent = transform.parent.GetComponent<Enemy>();
+        if (transform.parent.name == "Enemy(Clone)")
+        {
+            _enemyParent = transform.parent.GetComponent<Enemy>();
+            _parentID = 0;
+        }
 
         if (_enemyParent == null)
         {
-            Debug.LogError("Powerup Detection enemy script reference is NULL!");
+            Debug.LogError("Powerup Detection parent script reference is NULL!");
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_hasTarget == true)
+        if (_hasTarget == true && _parentID == 0)
         {
             _enemyParent.AttackPowerup(_powerupTarget.transform.position);
         }
