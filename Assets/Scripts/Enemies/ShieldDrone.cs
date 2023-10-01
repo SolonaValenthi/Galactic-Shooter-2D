efc1ca92fc9bcc46cdca27f5fc33188b5c78b3ae
Nikolai Nelson <nikolainelson@comcast.net>
2023-10-01 05:17:47 -0700

    boss drones now face the player during orbit

--- Assets/Scripts/Enemies/ShieldDrone.cs
+++ Assets/Scripts/Enemies/ShieldDrone.cs
@@ -6,16 +6,21 @@ public class ShieldDrone : MonoBehaviour
 {
     [SerializeField]
     private GameObject _basicLaser;
+    [SerializeField]
+    private GameObject _spriteController;
 
     private float _speed = 4;
+    private float _fireAngle;
     private bool _isRotating = false;
     private GameObject _boss;
+    private GameObject _playerObj;
     private BossAI _bossAI;
 
     // Start is called before the first frame update
     void Start()
     {
         _boss = GameObject.FindGameObjectWithTag("EnemyBoss");
+        _playerObj = GameObject.Find("Player");
         _bossAI = _boss.GetComponent<BossAI>();
 
         if (_boss == null)
@@ -33,11 +38,19 @@ public class ShieldDrone : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CaclulateFireAngle();
+
         if (_isRotating == true)
         {
             transform.RotateAround(_boss.transform.position, Vector3.forward, 36 * Time.deltaTime);
-        }
-        
+            _spriteController.transform.rotation = Quaternion.Euler(Vector3.back * _fireAngle);
+        }       
+    }
+
+    private void CaclulateFireAngle()
+    {
+        Vector3 targetPos = _playerObj.transform.position - transform.position;
+        _fireAngle = Mathf.Atan2(targetPos.x, targetPos.y) * Mathf.Rad2Deg - 180;
     }
 
     IEnumerator OnSpawn()
