c70db759080e4514b9cb80c90f41bf81c53d76dc
Nikolai Nelson <nikolainelson@comcast.net>
2023-10-11 11:31:23 -0700

    extended control display to title screen

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _titleDisplay;
+    [SerializeField]
+    private GameObject _controlDisplay;
+
     public void LoadGame()
     {
         SceneManager.LoadScene(1); //load the game scene
@@ -13,5 +18,17 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void DisplayControls()
+    {
+        _titleDisplay.SetActive(false);
+        _controlDisplay.SetActive(true);
+    }
+
+    public void ReturnToTitle()
+    {
+        _titleDisplay.SetActive(true);
+        _controlDisplay.SetActive(false);
     }
 }
